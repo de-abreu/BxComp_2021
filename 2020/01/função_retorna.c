7101fd99ffc6151aaa0cf1320dbe55f2755ef226
Kennedy Menezes <meneze_kennedy@hotmail.com>
2021-09-07 14:33:10 -0300

    input e switch

---
+++ 2020/01/função_retorna.c
@@ -0,0 +1,25 @@
+int main () {
+
+    int n;
+    float litro, teor;
+    
+    
+    printf("Quantos barris você quer balancear?");
+    scanf("%d", &n);
+
+    for(int i=0; i <= n; i++) {
+        scanf("%f %f", &litro, &teor);
+    }
+
+    // switch (expression)
+    // {
+    // case /* constant-expression */:
+    //     /* code */
+    //     break;
+    
+    // default:
+    //     break;
+    // }
+
+
+}
