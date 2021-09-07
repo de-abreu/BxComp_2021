f56f4a0988c076e30f340b8857cfc632fd346d08
Kennedy Menezes <meneze_kennedy@hotmail.com>
2021-09-07 14:41:19 -0300

    criando função_balancear

--- 2020/01/função_retorna.c
+++ 2020/01/função_retorna.c
@@ -1,25 +1,28 @@
-int main () {
+int funcaoBalancear () {
 
     int n;
     float litro, teor;
-    
     
     printf("Quantos barris você quer balancear?");
     scanf("%d", &n);
 
     for(int i=0; i <= n; i++) {
         scanf("%f %f", &litro, &teor);
+
+        switch ()
+        {
+        case:
+            break;
+        default:
+            break;
+        }
     }
 
-    // switch (expression)
-    // {
-    // case /* constant-expression */:
-    //     /* code */
-    //     break;
-    
-    // default:
-    //     break;
-    // }
 
+}
+
+int main () {
+
+    funcaoBalancear();
 
 }
