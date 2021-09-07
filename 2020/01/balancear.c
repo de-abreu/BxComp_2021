672ded260c3988b02d76af484cc7985af77dd311
Abreu <abreu@disroot.org>
2021-09-07 15:07:43 -0300

    Rename main

---
+++ 2020/01/balancear.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int balancear() {
+    int i, barrels[4], sortedBarrels[4], reservoir[2], isBigger;
+
+    for (i = 0; i < 4; i++)
+        if (!scanf(" %f", barrels[i]) || barrels[i] <= 0)
+            return -1;
+
+    reservoir[0] = barrels[0] + barrels[2]
+
+    sortedBarrels = sortBarrels(barrels)
+    isBigger = (barrels[1] == sortedBarrels[1]) ? 1 : 2;
+
+    for (i = 0; i < 4; i += 2) {
+
+    }
+}
